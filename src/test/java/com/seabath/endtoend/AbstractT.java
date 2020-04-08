76b7014728bef3b881d4e4172e55012ef37fbc41
sfontanel <sfontanel@deezer.com>
2020-04-08 14:34:24 +0200

    FEATURE: init project

---
+++ src/test/java/com/seabath/endtoend/AbstractT.java
@@ -0,0 +1,19 @@
+package com.seabath.endtoend;
+
+import org.junit.After;
+import org.junit.jupiter.api.BeforeEach;
+
+public class AbstractT {
+
+
+    @BeforeEach
+    public void initTest() {
+
+    }
+
+
+    @After
+    public void tearDownTest() {
+
+    }
+}
