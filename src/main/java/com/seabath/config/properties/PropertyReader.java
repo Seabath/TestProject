4fc420f1ea708e0084a735e45070bbf462f8ee0e
sfontanel <sfontanel@deezer.com>
2020-04-09 15:35:48 +0200

    FEATURE: addition of properties to allure repport

--- src/main/java/com/seabath/config/properties/PropertyReader.java
+++ src/main/java/com/seabath/config/properties/PropertyReader.java
@@ -5,10 +5,14 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class PropertyReader {
     public static final String PARAM_FILE_PATH = "paramFilePath";
+    private static final String PATH_ALLURE_RESULT = System.getProperty("allure.results.directory");
+    public static final String PATH_TO_ALLURE_ENVIRONNEMENT_PROPERTIES =
+        PATH_ALLURE_RESULT + "/environment.properties";
 
     private final String propertyFilePath;
 
@@ -22,10 +26,10 @@ public class PropertyReader {
         final File file = new File(propertyFilePath);
         try {
             properties.load(new FileInputStream(file));
+            FileUtils.copyFile(file, new File(PATH_TO_ALLURE_ENVIRONNEMENT_PROPERTIES));
         } catch (IOException e) {
             throw new IllegalStateException("Could not read file: " + file, e);
         }
-
         return new TestParam<>(properties);
     }
 }
